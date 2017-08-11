9375a3dac98a689671da88bb145eb07b78ff1e61
Hack Reactor Student <student@hackreactor.com>
2017-08-11 16:09:37 -0500

    Davis project

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-  // YOUR JAVASCRIPT/JQUERY CODE GOES HERE!
-
+   $("form").submit(function(event){
+    event.preventDefault()
+    var user1Inputty = $("#userInputty").val()
+    console.log(user1Inputty)
+    $("#user").append('<li>' + user1Inputty + '</li>');
+    $("#userInputty").val("");
+    })
+     $("#user").click(function(event) {
+       var itemToRemove = event.target;
+       console.log(itemToRemove);
+       $(itemToRemove).remove();
+     });
 
 })
